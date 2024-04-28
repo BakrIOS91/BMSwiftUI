da585062bfbe88bea7bb6829bf83f65497a2d868
Bakr Mohamed <Engbakrmohamed@hotmail.com>
2024-04-28 16:33:54 +0300

    Add Double Extension

---
+++ Sources/BMSwiftUI/Extensions/Double+Ext.swift
@@ -0,0 +1,24 @@
+//
+//  Double+Ext.swift
+//
+//
+//  Created by Bakr mohamed on 28/04/2024.
+//
+
+import Foundation
+
+extension Double {
+    /// Rounds the double to decimal places value
+    func toStringRounded(toPlaces places: Int) -> String {
+        return String(format: "%.\(places)f", self)
+    }
+    
+    func round(to places: Int) -> Double {
+        let divisor = pow(10.0, Double(places))
+        return (self * divisor).rounded() / divisor
+    }
+    
+    var toInt: Int {
+        return Int(self)
+    }
+}
