db10dfffa02570979758602c340afa06092b8748
Bakr Mohamed <engbakrmohamed@hotmail.com>
2024-08-03 20:15:20 +0300

    try new bundle configurations

--- Sources/BMSwiftUI/Extensions/Bundle+Ext.swift
+++ Sources/BMSwiftUI/Extensions/Bundle+Ext.swift
@@ -9,43 +9,42 @@ import Foundation
 
 private final class BundleToken {
     static var kBundleKey: UInt8 = 0
+    
+    static let bundle: Bundle = {
+        // Use the main bundle for the project
+        return Bundle.main
+    }()
 }
 
-public final class BundleExtension: Bundle {
-    public static var shared = BundleExtension()
+final class BundleExtension: Bundle {
+    static let shared = BundleExtension()
     
-    public override func localizedString(forKey key: String, value: String?, table tableName: String?) -> String {
+    override func localizedString(forKey key: String, value: String? = nil, table tableName: String? = nil) -> String {
         guard let bundle = objc_getAssociatedObject(self, &BundleToken.kBundleKey) as? Bundle else {
-            NSLog("Using Bundle.main for localization")
-            return super.localizedString(forKey: key, value: value, table: tableName)
+            return Bundle.main.localizedString(forKey: key, value: value, table: tableName)
         }
         
         // Fetch localized string from custom bundle
         let localizedString = bundle.localizedString(forKey: key, value: value, table: tableName)
         
         // Debug NSLog
-        NSLog("Localized String: \(localizedString) for key: \(key)")
+        NSLog("Localized string: \(localizedString)")
         return localizedString
     }
 }
 
 public extension Bundle {
-    static private var appLanguageDefaultsKey: String = "kAppLanguage"
-    static private var bundleType: String = "lproj"
-    
     /// Override the main bundle class (once in the app life) to make the new localizedString function work
     static let onceAction: Void = {
-        NSLog("Overriding Bundle.main with BundleExtension")
         object_setClass(Bundle.main, BundleExtension.self)
     }()
     
     static func setLanguage(language: String) {
         Bundle.onceAction
-        UserDefaults.standard.set([language], forKey: appLanguageDefaultsKey)
+        UserDefaults.standard.set([language], forKey: "AppleLanguages")
         UserDefaults.standard.synchronize()
         
-        guard let path = Bundle.main.path(forResource: language, ofType: bundleType) else {
-            NSLog("Failed to find path for resource: \(language).\(bundleType)")
+        guard let path = Bundle.main.path(forResource: language, ofType: "lproj") else {
             return
         }
         
