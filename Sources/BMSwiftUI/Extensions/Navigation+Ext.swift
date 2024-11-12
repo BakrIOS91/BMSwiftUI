81a5cf7dae9814d33d8df22351f2713031d5fa43
Bakr Mohamed <engbakrmohamed@hotmail.com>
2024-11-12 18:50:28 +0200

    Fix Dismiss Navigation On Swipe

---
+++ Sources/BMSwiftUI/Extensions/Navigation+Ext.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  BMSwiftUI
+//
+//  Created by Bakr mohamed on 12/11/2024.
+//
+
+import Foundation
+import UIKit
+
+public extension UINavigationController: UIGestureRecognizerDelegate {
+    open override func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        // Enable the gesture only if there is more than one view controller on the stack
+        return viewControllers.count > 1
+    }
+}
