d5259a5c248550ef2a1d4bf908ddd79e3436cecb
Bakr Mohamed <>
2025-09-02 19:35:02 +0300

    Another onfirstApear modifire

--- Sources/BMSwiftUI/Modifires/OnFirstAppear.swift
+++ Sources/BMSwiftUI/Modifires/OnFirstAppear.swift
@@ -12,10 +12,32 @@ public extension View {
     func onFirstAppear(perform action: @escaping () -> Void) -> some View {
         modifier(FirstAppearModifier(action: action))
     }
+    
+    func onFirstAppear(
+            hasAppeared: Binding<Bool>,
+            perform action: @escaping () -> Void
+        ) -> some View {
+            modifier(FirstAppearModifierTwo(hasAppeared: hasAppeared, action: action))
+        }
 }
 
 public struct FirstAppearModifier: ViewModifier {
     @State private var hasAppeared = false
+    let action: () -> Void
+    
+    public func body(content: Content) -> some View {
+        content.onAppear {
+            if !hasAppeared {
+                hasAppeared = true
+                action()
+            }
+        }
+    }
+}
+
+
+public struct FirstAppearModifierTwo: ViewModifier {
+    @Binding var hasAppeared: Bool
     let action: () -> Void
 
     public func body(content: Content) -> some View {
