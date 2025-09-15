76a8775cd5b24094b73a70276e9fcdd11c5b72a8
Sherif Ashraf <sherifashraf910@gmail.com>
2025-09-15 22:17:20 +0300

    adjust scaling factor

--- Sources/BMSwiftUI/Helpers/DeviceHelper.swift
+++ Sources/BMSwiftUI/Helpers/DeviceHelper.swift
@@ -8,26 +8,20 @@
 import SwiftUI
 
 public struct DeviceHelper {
+    /// Returns a scaling factor relative to iPhone 14 width (390pt).
     public static func getScalingFactor() -> CGFloat {
-        let nativeWidth = UIScreen.main.nativeBounds.width
+        let idiom = UIDevice.current.userInterfaceIdiom
         
-        // Set scaling factor based on actual screen resolution in pixels
-        if UIDevice.current.userInterfaceIdiom == .pad {
-            return 1.5 // iPad scaling factor
-        } else if nativeWidth <= 640 { // iPhone SE (1st, 2nd and 3rd gen), iPhone 5, 5s, 5c (640 x 1136)
-            return 0.75 // Scale down for smaller iPhones
-        } else if nativeWidth <= 750 { // iPhone 6, 6s, 7, 8 (750 x 1334)
-            return 0.8 // Normal iPhone sizes
-        } else if nativeWidth <= 1080 { // iPhone 6+/7+/8+ (1080 x 1920 / 1242 x 2208)
-            return 0.85 // Slightly larger scaling for Plus models
-        } else if nativeWidth <= 1125 { // iPhone X, XS (1125 x 2436)
-            return 0.9 // Scaling for notch iPhones
-        } else if nativeWidth <= 1242 { // iPhone XS Max (1242 x 2688)
-            return 0.95 // Larger iPhone scaling
-        } else if nativeWidth <= 1284 { // iPhone 12 Pro Max, 13 Pro Max (1284 x 2778)
-            return 0.95 // Largest iPhones scaling
-        } else {
-            return 1.0 // Default scaling factor for newer iPhones not listed
+        if idiom == .pad {
+            return 1.5
         }
+        
+        let screenWidth = UIScreen.main.bounds.width
+        let referenceWidth: CGFloat = 440  // iPhone 16 Pro max width in points
+        
+        // scale relative to baseline
+        let scale = screenWidth / referenceWidth
+        
+        return min(scale, 1.1)
     }
 }
